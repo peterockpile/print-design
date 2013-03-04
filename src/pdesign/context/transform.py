d0546626ba0a4ab11701801da9061d7798157804
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-03-04 14:32:04 +0000

    context panel

---
+++ src/pdesign/context/transform.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2013 by Igor E. Novikov
+#	
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gtk
+
+
+class ActionPlugin(gtk.HBox):
+
+	def __init__(self, mw):
+		gtk.HBox.__init__(self)
+		self.mw = mw
+		self.app = mw.app
+		self.actions = self.app.actions
+		self.sep = gtk.VSeparator()
+		self.pack_end(self.sep, False, False, 5)
+		self.build()
+
+	def build(self):pass
+
+class TestPlugin(ActionPlugin):
+
+	name = 'TestPlugin'
+
+	def build(self):
+		self.pack_start(gtk.Label(self.name))
+
+class Test1Plugin(ActionPlugin):
+
+	name = 'Test1Plugin'
+
+	def build(self):
+		self.pack_start(gtk.Label(self.name))
