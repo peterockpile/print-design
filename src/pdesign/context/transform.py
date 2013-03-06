112a3efbd2fe50668e1ca47f945e39f87819d4ef
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-03-06 14:53:37 +0000

    resize plugin refactored in own module

--- src/pdesign/context/transform.py
+++ src/pdesign/context/transform.py
@@ -17,8 +17,7 @@
 
 import gtk
 
-from pdesign import events
-from pdesign.widgets import ActionButton, UnitLabel, UnitSpin, KeepRatioLabel
+from pdesign.widgets import ActionButton
 
 class ActionPlugin(gtk.HBox):
 
@@ -77,95 +76,5 @@ class ToCurvePlugin(ActionPlugin):
 		self.but = ActionButton(self.actions['CONVERT_TO_CURVES'])
 		self.pack_start(self.but, False, False, 0)
 
-class ResizePlugin(ActionPlugin):
-
-	name = 'ResizePlugin'
-
-	def __init__(self, mw):
-		ActionPlugin.__init__(self, mw)
-		events.connect(events.NO_DOCS, self.set_state)
-		events.connect(events.DOC_CHANGED, self.set_state)
-		events.connect(events.SELECTION_CHANGED, self.set_state)
-
-	def build(self):
-		label = gtk.Label('Size:')
-		self.pack_start(label, False, False, 2)
-
-		self.width_spin = UnitSpin(self.user_changes)
-		self.pack_start(self.width_spin, False, False, 0)
-
-		label = gtk.Label('x')
-		self.pack_start(label, False, False, 0)
-
-		self.height_spin = UnitSpin(self.user_changes)
-		self.pack_start(self.height_spin, False, False, 0)
-
-		label = UnitLabel()
-		self.pack_start(label, False, False, 2)
-
-		self.keep_ratio = KeepRatioLabel()
-		self.pack_start(self.keep_ratio, False, False, 5)
-
-	def set_state(self, *args):
-		if self.app.current_doc is None:
-			self.deactivate()
-		else:
-			if self.app.current_doc.selection.objs:
-				self.activate()
-			else:
-				self.deactivate()
-
-	def deactivate(self):
-		self.width_spin.set_point_value(0)
-		self.height_spin.set_point_value(0)
-		self.set_sensitive(False)
-
-	def activate(self):
-		bbox = self.app.current_doc.selection.bbox
-		w = bbox[2] - bbox[0]
-		h = bbox[3] - bbox[1]
-		self.width_spin.set_point_value(w)
-		self.height_spin.set_point_value(h)
-		self.set_sensitive(True)
-
-	def user_changes(self, *args):
-		doc = self.app.current_doc
-		if not doc is None and doc.selection.objs:
-			bbox = doc.selection.bbox
-			w = bbox[2] - bbox[0]
-			h = bbox[3] - bbox[1]
-			center_x = bbox[0] + w / 2.0
-			center_y = bbox[1] + h / 2.0
-
-			new_w = self.width_spin.get_point_value()
-			new_h = self.height_spin.get_point_value()
-
-			if not round(w, 4) == round(new_w, 4) or not round(h, 4) == round(new_h, 4):
-				if not new_w: self.width_spin.set_point_value(w);return
-				if not new_h: self.height_spin.set_point_value(h);return
-
-				m11 = round(new_w / w, 4)
-				m22 = round(new_h / h, 4)
-
-				if m11 == m22 == 1.0:return
-
-				trafo = []
-
-				if self.keep_ratio.value:
-					if m11 == 1.0:
-						dx = center_x * (1 - m22)
-						dy = center_y * (1 - m22)
-						trafo = [m22, 0.0, 0.0, m22, dx, dy]
-					if m22 == 1.0:
-						dx = center_x * (1 - m11)
-						dy = center_y * (1 - m11)
-						trafo = [m11, 0.0, 0.0, m11, dx, dy]
-				else:
-					dx = center_x * (1 - m11)
-					dy = center_y * (1 - m22)
-					trafo = [m11, 0.0, 0.0, m22, dx, dy]
-
-				if trafo:
-					doc.api.transform_selected(trafo)
 
 
