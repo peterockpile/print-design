c8ad101c04706869fadc41f42cfbc177d24509dd
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-03-12 12:09:51 +0000

    DOM viewer

--- src/pdesign/plugins/dom_plg.py
+++ src/pdesign/plugins/dom_plg.py
@@ -37,15 +37,115 @@ class DOMPlugin(gtk.VBox):
 		self.caption = PluginTabCaption(self, self.icon, self.title)
 
 	def build(self):
-		self.label = gtk.Label()
-		self.label.set_text(str(self.active))
-		self.pack_start(self.label)
+		spacer = gtk.VBox()
+		self.add(spacer)
+		self.set_border_width(5)
+
+		model = self.app.current_doc.doc_presenter.model
+		self.listmodel = ObjectTreeModel(model)
+
+		self.treeview = gtk.TreeView()
+
+		self.column = gtk.TreeViewColumn()
+		self.column.set_title(_('Document Object Model'))
+		render_pixbuf = gtk.CellRendererPixbuf()
+		self.column.pack_start(render_pixbuf, expand=False)
+		self.column.add_attribute(render_pixbuf, 'pixbuf', 0)
+		render_text = gtk.CellRendererText()
+		self.column.pack_start(render_text, expand=True)
+		self.column.add_attribute(render_text, 'text', 1)
+		self.treeview.append_column(self.column)
+
+		self.column1 = gtk.TreeViewColumn()
+		render_text = gtk.CellRendererText()
+		self.column1.pack_start(render_text, expand=False)
+		self.column1.add_attribute(render_text, 'text', 2)
+		self.column1.add_attribute(render_text, 'foreground', 3)
+		self.treeview.append_column(self.column1)
+
+		self.treeview.connect('cursor-changed', self.view_object)
+
+		self.scrolledwindow = gtk.ScrolledWindow()
+		self.scrolledwindow.add(self.treeview)
+		self.scrolledwindow.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+		self.scrolledwindow.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+		spacer.pack_end(self.scrolledwindow, True)
+
+		self.treeview.set_model(self.listmodel)
+		self.treeview.set_rules_hint(True)
+		self.treeview.set_enable_tree_lines(True)
+
+		self.expand_all()
+
 		self.loaded = True
+		events.connect(events.DOC_CHANGED, self.update_view)
+		events.connect(events.DOC_MODIFIED, self.update_view)
+
+	def update_view(self, *args):
+		if self.active:
+			model = self.app.current_doc.doc_presenter.model
+			self.listmodel = ObjectTreeModel(model)
+			self.treeview.set_model(self.listmodel)
+			self.expand_all()
 
 	def activate(self):
 		self.active = True
-		self.label.set_text(str(self.active))
+		self.update_view()
 
 	def deactivate(self):
 		self.active = False
-		self.label.set_text(str(self.active))
+
+	def collapse_all(self, *args):
+		self.treeview.collapse_all()
+
+	def expand_all(self, *args):
+		self.treeview.expand_all()
+
+	def view_object(self, *args):pass
+
+
+
+NODE_ICON = gtk.Image().render_icon(gtk.STOCK_DIRECTORY, gtk.ICON_SIZE_MENU)
+LEAF_ICON = gtk.Image().render_icon(gtk.STOCK_FILE, gtk.ICON_SIZE_MENU)
+COLOR = '#A7A7A7'
+
+class ObjectTreeModel(gtk.TreeStore):
+
+	def __init__(self, model):
+		gtk.TreeStore.__init__(self, gtk.gdk.Pixbuf, str, str, str)
+
+		self.model = model
+		self.model_dict = {}
+
+		iter = self.append(None)
+		self.add_to_dict(self.model, iter)
+		self.model_dict[iter] = self.model
+		icon_type, name, info = self.model.resolve()
+		self.set(iter, 0, self.get_icon(icon_type),
+						1, name,
+						2, info,
+						3, COLOR)
+		for child in self.model.childs:
+			self.scan_model(iter, child)
+
+	def scan_model(self, iter, obj):
+		child_iter = self.append(iter)
+		self.add_to_dict(obj, child_iter)
+		icon_type, name, info = obj.resolve()
+		self.set(child_iter, 0, self.get_icon(icon_type),
+							1, name,
+							2, info,
+							3, COLOR)
+		for item in obj.childs:
+			self.scan_model(child_iter, item)
+
+	def add_to_dict(self, obj, iter):
+		path_str = self.get_path(iter).__str__()
+		self.model_dict[path_str] = obj
+
+	def get_obj_by_path(self, path):
+		return self.model_dict[path.__str__()]
+
+	def get_icon(self, type):
+		if type: return LEAF_ICON
+		return NODE_ICON
