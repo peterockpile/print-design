c2b784062a13132bb2e14dbe9df9c63462167bb6
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-04-04 17:23:29 +0000

    Snapping manager template

---
+++ trunk/src/pdesign/view/snapping.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2013 by Igor E. Novikov
+#	
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+
+class SnapManager:
+
+	presenter = None
+	doc = None
+	methods = None
+
+	snap_to_grid = False
+	snap_to_guides = False
+	snap_to_objects = False
+
+	def __init__(self, presenter):
+
+		self.presenter = presenter
+		self.doc = self.presenter.doc_presenter
+		self.methods = self.presenter.methods
+
+	def snap_point(self, point):pass
+
+	def spap_bbox(self, bbox):pass
+
