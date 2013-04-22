aadd224d770ecec255752f3874ba9d571ceb6bd5
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-04-22 07:40:33 +0000

    guide dragging configs

--- src/pdesign/app_conf.py
+++ src/pdesign/app_conf.py
@@ -121,6 +121,9 @@ class AppConfig(UCConfig):
 	snap_line_dash = [5, 5]
 	snap_line_color = (1.0, 0.0, 0.0, 1.0)
 
+	guide_line_dash = [5, 5]
+	guide_line_dragging_color = (0.0, 0.0, 0.0, 0.25)
+
 	#============== BEZIER CURVE OPTIONS ================
 	curve_autoclose_flag = 0
 
