ca9e0f9f4e8b954cc6407373ae7e8b9ef5f74572
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-04-23 13:24:24 +0000

    prefs dialog and plugin infrastructure

--- src/pdesign/dialogs/prefs/__init__.py
+++ src/pdesign/dialogs/prefs/__init__.py
@@ -17,45 +17,173 @@
 
 import gtk
 
-from pdesign import _, config
+from pdesign import _, config, appconst
+from pdesign.dialogs.prefs import test
 
-DIALOG = None
-PREFS = None
+PLUGINS = [test.TestPlugin, test.Test1Plugin, test.Test2Plugin, test.Test3Plugin]
 
 def get_prefs_dialog(app):
+	parent = app.mw
+	title = _('%s Preferences') % (app.appdata.app_name)
 
-	global DIALOG, PREFS
-
-	if DIALOG is None:
-		parent = app.mw
-		title = _('%s Preferences') % (app.appdata.app_name)
-
-		DIALOG = gtk.Dialog(title, parent,
-		                   gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-		                   (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
-		                    gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+	dialog = gtk.Dialog(title, parent,
+	                   gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+	                   (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+	                    gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
 
 
-		vbox = gtk.VBox()
-		vbox.set_border_width(5)
-		vbox.show_all()
-		DIALOG.vbox.pack_start(vbox)
+	vbox = gtk.VBox()
+	content = PrefsContainer(app)
+	vbox.pack_start(content)
+	vbox.set_border_width(5)
+	vbox.show_all()
+	dialog.vbox.pack_start(vbox)
 
-	ret = DIALOG.run()
+	ret = dialog.run()
 	if ret == gtk.RESPONSE_ACCEPT:
 		app.proxy.force_redraw()
-	DIALOG.hide()
+	dialog.destroy()
 
 class PrefsContainer(gtk.HPaned):
 
+	model = None
+	plugins = []
+	current_plg = None
+
 	def __init__(self, app):
 		self.app = app
 		gtk.HPaned.__init__(self)
+		self.set_size_request(700, 400)
+		self.build_model()
+		self.viewer = PluginViewer(self)
+		self.add1(self.viewer)
+		self.load_plugin(self.plugins[0])
 
-class PrefsLeaf(gtk.VBox):
+	def build_model(self):
+		self.model = PrefsNode(_('Preferences'), gtk.STOCK_PREFERENCES)
+		self.app_prefs = PrefsNode(_('Application'), gtk.STOCK_PROPERTIES)
+		self.model.childs.append(self.app_prefs)
+		self.doc_prefs = PrefsNode(_('New document'), gtk.STOCK_NEW)
+		self.model.childs.append(self.doc_prefs)
+		for item in PLUGINS:
+			plg = item(self.app)
+			if plg.cid == appconst.PREFS_APP_PLUGIN:
+				self.app_prefs.childs.append(plg)
+			else:
+				self.doc_prefs.childs.append(plg)
+			self.plugins.append(plg)
 
+	def load_plugin(self, plg):
+		if not plg.leaf: return
+		if not self.current_plg is None: self.remove(self.current_plg)
+		if not plg.built:
+			plg.build()
+		self.add2(plg)
+		plg.show_all()
+		self.current_plg = plg
+
+
+class PrefsNode:
+
+	short_title = ''
+	icon = gtk.STOCK_DIRECTORY
 	childs = []
+	leaf = False
 
-	def __init__(self, app):
-		self.app = app
-		gtk.HPaned.__init__(self)
+	def __init__(self, title, icon=None):
+		self.childs = []
+		self.short_title = title
+		if not icon is None:
+			self.icon = icon
+
+class PluginViewer(gtk.VBox):
+
+	def __init__(self, owner):
+		gtk.VBox.__init__(self)
+		self.owner = owner
+		spacer = gtk.VBox()
+		self.add(spacer)
+		self.set_border_width(5)
+		self.set_size_request(200, -1)
+
+		self.listmodel = ObjectTreeModel(self.owner.model)
+
+		self.treeview = gtk.TreeView()
+
+		self.column = gtk.TreeViewColumn()
+		render_pixbuf = gtk.CellRendererPixbuf()
+		self.column.pack_start(render_pixbuf, expand=False)
+		self.column.add_attribute(render_pixbuf, 'pixbuf', 0)
+		render_text = gtk.CellRendererText()
+		self.column.pack_start(render_text, expand=True)
+		self.column.add_attribute(render_text, 'text', 1)
+		self.treeview.append_column(self.column)
+
+		self.treeview.connect('cursor-changed', self.view_object)
+
+		self.scrolledwindow = gtk.ScrolledWindow()
+		self.scrolledwindow.add(self.treeview)
+		self.scrolledwindow.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+		self.scrolledwindow.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+		spacer.pack_end(self.scrolledwindow, True)
+
+		self.treeview.set_model(self.listmodel)
+		self.treeview.set_rules_hint(True)
+		self.treeview.set_enable_tree_lines(True)
+
+		self.expand_all()
+
+		self.loaded = True
+
+	def update_view(self, *args):
+		if self.active:
+			self.listmodel = ObjectTreeModel(self.owner.model)
+			self.treeview.set_model(self.listmodel)
+			self.expand_all()
+
+	def activate(self):
+		self.active = True
+		self.update_view()
+
+	def deactivate(self):
+		self.active = False
+
+	def collapse_all(self, *args):
+		self.treeview.collapse_all()
+
+	def expand_all(self, *args):
+		self.treeview.expand_all()
+
+	def view_object(self, *args):
+		path = self.treeview.get_cursor()[0]
+		obj = self.listmodel.get_obj_by_path(path)
+		self.owner.load_plugin(obj)
+
+class ObjectTreeModel(gtk.TreeStore):
+
+	def __init__(self, model):
+		gtk.TreeStore.__init__(self, gtk.gdk.Pixbuf, str)
+
+		self.model = model
+		self.model_dict = {}
+		for child in self.model.childs:
+			self.scan_model(None, child)
+
+	def scan_model(self, iter, obj):
+		child_iter = self.append(iter)
+		self.add_to_dict(obj, child_iter)
+		self.set(child_iter, 0, self.get_icon(obj),
+							1, obj.short_title)
+		for item in obj.childs:
+			self.scan_model(child_iter, item)
+
+	def add_to_dict(self, obj, iter):
+		path_str = self.get_path(iter).__str__()
+		self.model_dict[path_str] = obj
+
+	def get_obj_by_path(self, path):
+		return self.model_dict[path.__str__()]
+
+	def get_icon(self, obj):
+		return gtk.Image().render_icon(obj.icon, gtk.ICON_SIZE_MENU)
+
