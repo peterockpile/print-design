e4265a231b5927b5aeebf2645e35d673ce64d379
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-05-25 14:05:25 +0000

    deb package build

--- trunk/setup.py
+++ trunk/setup.py
@@ -141,3 +141,20 @@ setup(name=NAME,
 #################################################
 libutils.compile_sources()
 
+#################################################
+# Implementation of bdist_deb command
+#################################################
+if DEB_PACKAGE:
+	bld = DEB_Builder(name=NAME,
+					version=VERSION,
+					maintainer='%s <%s>' % (AUTHOR, AUTHOR_EMAIL),
+					depends=deb_depends,
+					homepage=URL,
+					description=DESCRIPTION,
+					long_description=LONG_DEB_DESCRIPTION,
+					pkg_dirs=libutils.get_package_dirs().keys(),
+					scripts=scripts,
+					data_files=data_files)
+	bld.build()
+
+libutils.clear_build()
