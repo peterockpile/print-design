d2c841af6918e004659d17f22384a4eeb1851839
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-05-25 13:54:30 +0000

    project description

--- trunk/setup.py
+++ trunk/setup.py
@@ -42,3 +42,47 @@ from libutils import make_source_list, DEB_Builder
 
 #Flags
 DEB_PACKAGE = False
+
+############################################################
+#
+# Package description
+#
+############################################################
+NAME = 'printdesign'
+VERSION = '1.0'
+DESCRIPTION = 'Universal vector graphics translator'
+AUTHOR = 'Igor E. Novikov'
+AUTHOR_EMAIL = 'igor.e.novikov@gmail.com'
+MAINTAINER = AUTHOR
+MAINTAINER_EMAIL = AUTHOR_EMAIL
+LICENSE = 'GPL v3'
+URL = 'http://sk1project.org'
+DOWNLOAD_URL = 'http://sk1project.org'
+CLASSIFIERS = [
+'Development Status :: 5 - Stable',
+'Environment :: Desktop',
+'Intended Audience :: End Users/Desktop',
+'License :: OSI Approved :: LGPL v2',
+'License :: OSI Approved :: GPL v3',
+'Operating System :: POSIX',
+'Operating System :: MacOS :: MacOS X',
+'Programming Language :: Python',
+"Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+]
+LONG_DESCRIPTION = '''
+PrintDesign is an open source vector graphics editor similar to CorelDRAW, 
+Adobe Illustrator, or Freehand. PrintDesign is oriented for prepress industry, 
+so it works with CMYK colorspace and produces CMYK-based PDF and PS output. 
+
+sK1 Project (http://sk1project.org),
+Copyright (C) 2010-2013 by Igor E. Novikov
+'''
+LONG_DEB_DESCRIPTION = ''' .
+ PrintDesign is an open source vector graphics editor similar to CorelDRAW,
+ Adobe Illustrator, or Freehand. PrintDesign is oriented for prepress industry,
+ so it works with CMYK colorspace and produces CMYK-based PDF and PS output.
+ . 
+ sK1 Project (http://sk1project.org),
+ Copyright (C) 2010-2013 by Igor E. Novikov 
+ .
+'''
