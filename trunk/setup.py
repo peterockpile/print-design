97b1b02b2b5c393bd4b9260157b8984e2eb43166
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-05-25 13:45:03 +0000

    build template

---
+++ trunk/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Setup script for UniConvertor 2.x
+#
+# Copyright (C) 2013 Igor E. Novikov
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+
+"""
+Usage: 
+--------------------------------------------------------------------------
+ to build package:   python setup.py build
+ to install package:   python setup.py install
+--------------------------------------------------------------------------
+ to create source distribution:   python setup.py sdist
+--------------------------------------------------------------------------
+ to create binary RPM distribution:  python setup.py bdist_rpm
+--------------------------------------------------------------------------
+ to create binary DEB distribution:  python setup.py bdist_deb
+--------------------------------------------------------------------------
+
+help on available distribution formats: python setup.py bdist --help-formats
+"""
+
+import os, sys
+
+import libutils
+from libutils import make_source_list, DEB_Builder
+
+#Flags
+DEB_PACKAGE = False
