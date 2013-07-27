0731fa3809820ee51b56ef2749025b35b32fbe1e
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-07-27 17:30:18 +0000

    file dialogs are refactored in own module

--- trunk/src/pdesign/dialogs/__init__.py
+++ trunk/src/pdesign/dialogs/__init__.py
@@ -15,5 +15,6 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sysdlgs import msg_dialog, get_open_file_name, get_save_file_name
+from sysdlgs import msg_dialog
+from filedlgs import get_open_file_name, get_save_file_name
 from progressdlg import ProgressDialog
--- trunk/src/pdesign/dialogs/sysdlgs.py
+++ trunk/src/pdesign/dialogs/sysdlgs.py
@@ -15,18 +15,9 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import wx
 
-from uc2 import uc2const
-from uc2.formats import data
-
-from pdesign import _, config
-from pdesign.utils.fs import expanduser_unicode
-
-
-def msg_dialog(parent, title, text, seconary_text='', details='',
-			dlg_type=None):
+def msg_dialog(parent, title, text, seconary_text='', details='', dlg_type=None):
 	dlg = wx.MessageDialog(parent, text,
 					   title,
 					   wx.OK | wx.ICON_INFORMATION
@@ -35,81 +26,6 @@ def msg_dialog(parent, title, text, seconary_text='', details='',
 	dlg.ShowModal()
 	dlg.Destroy()
 
-def _get_open_filters():
-	wildcard = ''
-	descr = uc2const.FORMAT_DESCRIPTION
-	ext = uc2const.FORMAT_EXTENSION
-	items = [] + data.LOADER_FORMATS
-	wildcard += _('All supported formats') + '|'
-	for item in items:
-		for extension in ext[item]:
-			wildcard += '*.' + extension + ';'
-			wildcard += '*.' + extension.upper() + ';'
-	wildcard += '|'
-
-	wildcard += _('All files (*.*)') + '|'
-	wildcard += '*;*.*|'
-
-	for item in items:
-		wildcard += descr[item] + '|'
-		for extension in ext[item]:
-			wildcard += '*.' + extension + ';'
-			wildcard += '*.' + extension.upper() + ';'
-		wildcard += '|'
-
-	return wildcard
-
-def get_open_file_name(parent, app, start_dir):
-	ret = ''
-	msg = _('Open document')
-
-	start_dir = expanduser_unicode(start_dir)
-
-	dlg = wx.FileDialog(
-		parent, message=msg,
-		defaultDir=start_dir,
-		defaultFile="",
-		wildcard=_get_open_filters(),
-		style=wx.OPEN | wx.CHANGE_DIR | wx.FILE_MUST_EXIST
-		)
-	dlg.CenterOnParent()
-	if dlg.ShowModal() == wx.ID_OK:
-		ret = dlg.GetPath()
-	dlg.Destroy()
-	return ret
-
-def _get_save_fiters():
-	wildcard = ''
-	descr = uc2const.FORMAT_DESCRIPTION
-	ext = uc2const.FORMAT_EXTENSION
-	items = [] + data.SAVER_FORMATS
-	for item in items:
-		wildcard += descr[item] + '|'
-		for extension in ext[item]:
-			wildcard += '*.' + extension + ';'
-			wildcard += '*.' + extension.upper() + ';'
-		wildcard += '|'
-	return wildcard
-
-def get_save_file_name(parent, app, path):
-	ret = ''
-	msg = _('Save document As...')
-
-	path = expanduser_unicode(path)
-	doc_folder = os.path.dirname(path)
-
-	dlg = wx.FileDialog(
-		parent, message=msg,
-		defaultDir=doc_folder,
-		defaultFile=path,
-		wildcard=_get_save_fiters(),
-		style=wx.SAVE | wx.CHANGE_DIR | wx.FD_OVERWRITE_PROMPT
-	)
-	dlg.CenterOnParent()
-	if dlg.ShowModal() == wx.ID_OK:
-		ret = dlg.GetPath()
-	dlg.Destroy()
-	return ret
 
 
 
