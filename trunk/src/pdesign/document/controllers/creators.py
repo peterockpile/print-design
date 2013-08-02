d696f1de5c81a96d409897fbbce0157ff2f570fa
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-08-02 17:02:22 +0000

    rect, ellipse and polygon creators

---
+++ trunk/src/pdesign/document/controllers/creators.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2013 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pdesign import modes
+from generic import AbstractController
+
+class AbstractCreator(AbstractController):
+
+	check_snap = True
+
+	def __init__(self, canvas, presenter):
+		AbstractController.__init__(self, canvas, presenter)
+
+	def mouse_down(self, event):
+		AbstractController.mouse_down(self, event)
+
+	def mouse_move(self, event):
+		if self.draw:
+			AbstractController.mouse_move(self, event)
+		else:
+			self.counter += 1
+			if self.counter > 5:
+				self.counter = 0
+				point = list(event.GetPositionTuple())
+				dpoint = self.canvas.win_to_doc(point)
+				if self.selection.is_point_over_marker(dpoint):
+					mark = self.selection.is_point_over_marker(dpoint)[0]
+					self.canvas.resize_marker = mark
+					self.canvas.set_temp_mode(modes.RESIZE_MODE)
+
+class RectangleCreator(AbstractCreator):
+
+	mode = modes.RECT_MODE
+
+	def __init__(self, canvas, presenter):
+		AbstractCreator.__init__(self, canvas, presenter)
+
+	def do_action(self, event):
+		if self.start and self.end:
+			if abs(self.end[0] - self.start[0]) > 2 and \
+			abs(self.end[1] - self.start[1]) > 2:
+				rect = self.start_doc + self.end_doc
+				self.api.create_rectangle(rect)
+
+class EllipseCreator(AbstractCreator):
+
+	mode = modes.ELLIPSE_MODE
+
+	def __init__(self, canvas, presenter):
+		AbstractCreator.__init__(self, canvas, presenter)
+
+	def do_action(self, event):
+		if self.start and self.end:
+			if abs(self.end[0] - self.start[0]) > 2 and \
+			abs(self.end[1] - self.start[1]) > 2:
+				rect = self.start_doc + self.end_doc
+				self.api.create_ellipse(rect)
+
+class PolygonCreator(AbstractCreator):
+
+	mode = modes.POLYGON_MODE
+
+	def __init__(self, canvas, presenter):
+		AbstractCreator.__init__(self, canvas, presenter)
+
+	def do_action(self, event):
+		if self.start and self.end:
+			if abs(self.end[0] - self.start[0]) > 2 and \
+			abs(self.end[1] - self.start[1]) > 2:
+				rect = self.start_doc + self.end_doc
+				self.api.create_polygon(rect)
