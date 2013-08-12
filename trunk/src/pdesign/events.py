5e1859bafc7aadb0906dee0dbddf20f29a7440f5
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-08-12 15:19:47 +0000

    mouse monitor event

--- trunk/src/pdesign/events.py
+++ trunk/src/pdesign/events.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2011-2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 The package provides Qt-like signal-slot functionality
@@ -22,6 +22,7 @@ for internal events processing.
 Signal arguments:
 CONFIG_MODIFIED   arg[0] - [attr, value]
 APP_STATUS		  arg[0] - statusbar message
+MOUSE_STATUS	  arg[0] - mouse status tuple
 CMS_CHANGED		  no args 
 NO_DOCS		      no args
 DOC_MODIFIED	  arg[0] - presenter instance
@@ -39,6 +40,7 @@ PAGE_CHANGED      arg[0] - presenter instance
 CONFIG_MODIFIED = ['CONFIG_MODIFIED']
 
 APP_STATUS = ['APP_STATUS']
+MOUSE_STATUS = ['MOUSE_STATUS']
 CMS_CHANGED = ['CMS_CHANGED']
 
 NO_DOCS = ['NO_DOCS']
