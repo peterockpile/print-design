7d7730d8b67f853e856e69dd37cd35b30b7b6a2b
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-08-19 16:39:42 +0000

    test improved

--- trunk/src/ui-tests/cairo_test.py
+++ trunk/src/ui-tests/cairo_test.py
@@ -29,6 +29,9 @@ class CairoCanvas(VPanel):
 	drawing = False
 	start = []
 	end = []
+	new_end = []
+	surface = None
+	temp_surface = None
 
 	def __init__(self, parent):
 		VPanel.__init__(self, parent)
@@ -36,42 +39,103 @@ class CairoCanvas(VPanel):
 		self.Bind(wx.EVT_LEFT_DOWN, self.mouse_down)
 		self.Bind(wx.EVT_LEFT_UP, self.mouse_up)
 		self.Bind(wx.EVT_MOTION, self.mouse_move)
+		self.timer = wx.Timer(self)
+		self.Bind(wx.EVT_TIMER, self.on_timer)
 
 	def mouse_down(self, event):
 		self.drawing = True
 		self.start = list(event.GetPositionTuple())
 		self.end = list(event.GetPositionTuple())
+		self.timer.Start(50)
 		print 'down'
 
 	def mouse_up(self, event):
-		self.render_frame()
+		self.timer.Stop()
+		self.clear_frame()
 		self.drawing = False
-		self.start = self.end = []
+		self.start = self.end = self.new_end = []
 		print 'up'
 
 	def mouse_move(self, event):
 		if self.drawing:
-			self.render_frame()
-			self.end = list(event.GetPositionTuple())
-			self.render_frame()
+			self.new_end = list(event.GetPositionTuple())
+
+	def on_timer(self, event):
+		if not self.drawing: return
+		if self.start and self.end:
+			if self.new_end:
+				self.clear_frame()
+				self.end = self.new_end
+				self.new_end = []
+				self.render_frame()
+
+	def norm_rect(self, start, end):
+		x0, y0 = self.start
+		x1, y1 = self.end
+		x_min = min(x0, x1)
+		x_max = max(x0, x1)
+		y_min = min(y0, y1)
+		y_max = max(y0, y1)
+		w = x_max - x_min
+		h = y_max - y_min
+		return [x_min, y_min, w, h]
 
 	def render_frame(self):
 		if self.start and self.end:
-			x, y = self.start
-			x1, y1 = self.end
+			x, y, w, h = self.norm_rect(self.start, self.end)
+			if not w: w = 1
+			if not h: h = 1
+			temp_surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
+			ctx = cairo.Context(temp_surface)
+			ctx.set_source_surface(self.surface, -x, -y)
+			ctx.paint()
+			ctx.set_antialias(cairo.ANTIALIAS_NONE)
+			ctx.set_line_width(1.0)
+			ctx.set_source_rgb(1, 1, 1)
+			ctx.rectangle(1, 1, w - 1, h - 1)
+			ctx.stroke()
+			ctx.set_dash([5, 5])
+			ctx.set_source_rgb(0, 0, 0)
+			ctx.rectangle(1, 1, w - 1, h - 1)
+			ctx.stroke()
 			dc = wx.ClientDC(self)
-			dc.SetLogicalFunction(wx.INVERT)
-			pen = wx.Pen(wx.Colour(0, 0, 0), 1)
-			#pen.SetDashes([5, 5, 5, 5])
-			dc.SetPen(pen)
-			dc.SetBrush(wx.TRANSPARENT_BRUSH)
-			dc.DrawRectangle(x, y, x1 - x, y1 - y)
+			dc.DrawBitmap(copy_surface_to_bitmap(temp_surface), x, y)
+			rects = [[0, 0, 1, h - 1], [0, 0, w - 1, 1],
+				[w - 1, 0, 1, h - 1], [0, h - 1, w - 1, 1]]
+			for rect in rects:
+				x0, y0, w, h = rect
+				if not w: w = 1
+				if not h: h = 1
+				surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
+				ctx = cairo.Context(surface)
+				ctx.set_source_surface(temp_surface, -x0, -y0)
+				ctx.paint()
+				dc.DrawBitmap(copy_surface_to_bitmap(surface), x0 + x, y0 + y)
+
+	def clear_frame(self):
+		if self.start and self.end:
+			x, y, w, h = self.norm_rect(self.start, self.end)
+			dc = wx.ClientDC(self)
+			rects = [[x, y, 1, h], [x, y, w, 1],
+					[x + w - 1, y, 1, h], [x, y + h - 1, w, 1]]
+			for rect in rects:
+				x0, y0, w, h = rect
+				if not w: w = 1
+				if not h: h = 1
+				surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
+				ctx = cairo.Context(surface)
+				ctx.set_source_surface(self.surface, -x0, -y0)
+				ctx.paint()
+				dc.DrawBitmap(copy_surface_to_bitmap(surface), x0, y0)
 
 	def on_paint(self, event):
-		dc = wx.BufferedPaintDC(self)
-		width, height = dc.GetSize()
-		surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
-		ctx = cairo.Context(surface)
+		dc = wx.PaintDC(self)
+		if self.surface is None: self.draw_content(*dc.GetSize())
+		dc.DrawBitmap(copy_surface_to_bitmap(self.surface), 0, 0)
+
+	def draw_content(self, width, height):
+		self.surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
+		ctx = cairo.Context(self.surface)
 		ctx.set_source_rgb(1, 1, 1)
 		ctx.paint()
 		ctx.set_line_width(15)
@@ -81,7 +145,6 @@ class CairoCanvas(VPanel):
 		ctx.close_path()
 		ctx.set_source_rgba(0, 0, 0.5, 1)
 		ctx.stroke()
-		dc.DrawBitmap(copy_surface_to_bitmap(surface), 0, 0, True)
 
 app = Application('Cairo test')
 mw = MainWindow(app.app_name, (700, 500))
