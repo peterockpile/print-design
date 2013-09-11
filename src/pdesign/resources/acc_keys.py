f9fb5b60ce0b0f9dfdc4c7b451dbadaab981fa9a
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-09-11 12:04:07 +0000

    duplicate action resources

--- src/pdesign/resources/acc_keys.py
+++ src/pdesign/resources/acc_keys.py
@@ -33,9 +33,10 @@ wx.ID_CUT:(wx.ACCEL_CTRL, ord('X')),
 wx.ID_COPY:(wx.ACCEL_CTRL, ord('C')),
 wx.ID_PASTE:(wx.ACCEL_CTRL, ord('V')),
 wx.ID_DELETE:(wx.ACCEL_NORMAL, wx.WXK_DELETE),
+pdids.ID_DUPLICATE:(wx.ACCEL_CTRL, ord('D')),
 wx.ID_SELECTALL:(wx.ACCEL_CTRL, ord('A')),
-#----- View menu
 pdids.ID_DESELECT:(wx.ACCEL_SHIFT | wx.ACCEL_CTRL, ord('A')),
+#----- View menu
 pdids.ID_STROKE_VIEW:(wx.ACCEL_SHIFT, wx.WXK_F9),
 wx.ID_ZOOM_IN:(wx.ACCEL_CTRL, ord('=')),
 wx.ID_ZOOM_OUT:(wx.ACCEL_CTRL, ord('-')),
