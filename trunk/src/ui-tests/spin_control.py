c0e0f8504a5b5c2afdf4942bc91b1b6bbb034cc8
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-09-12 15:50:11 +0000

    test improved

--- trunk/src/ui-tests/spin_control.py
+++ trunk/src/ui-tests/spin_control.py
@@ -22,29 +22,58 @@ import wx
 from pdesign.widgets import const, BOTTOM, CENTER, LEFT, ALL, EXPAND
 from pdesign.widgets import Application, MainWindow, Entry
 from pdesign.widgets import HPanel, VPanel
-from pdesign.widgets.basic import SpinButton
+from pdesign.widgets.basic import SpinButton, SizedPanel
+from pdesign.widgets.generic import RangeDataWidget
 
-class SpinControl(wx.Panel):
+class FloatSpin(wx.Panel, RangeDataWidget):
 
-	def __init__(self, parent):
+	entry = None
+	sb = None
+
+	flag = True
+	value = 0.0
+	range_val = (0.0, 1.0)
+	step = 0.01
+	digits = 2
+	callback = None
+
+	def __init__(self, parent, value=0.0, range_val=(0.0, 1.0), step=0.01,
+				digits=2, size=const.DEF_SIZE, width=0, spin_overlay=True,
+				onchange=None, check_focus=True):
+
+		self.callback = onchange
+		if const.is_mac(): spin_overlay = False
+
 		wx.Panel.__init__(self, parent)
-		self.SetBackgroundColour(wx.RED)
-		if const.is_gtk():
-			self.entry = Entry(self)
+		#self.SetBackgroundColour(wx.RED)
+		if spin_overlay:
+			if const.is_gtk():
+				self.entry = Entry(self, '', size=size, width=width)#,
+						#onchange=self._check_entry, onenter=self._entry_enter)
+				size = (-1, self.entry.GetSize()[1])
+				self.sb = SpinButton(self, size=size)
+				w_pos = self.entry.GetSize()[0] - 5
+				self.sb.SetPosition((w_pos, -1))
+				line = HPanel(self)
+				line.SetSize((1, self.sb.GetSize()[1] - 1))
+				line.set_bg(const.UI_COLORS['dark_shadow'])
+				line.SetPosition((w_pos - 1, -1))
+			elif const.is_msw():
+				self.entry = Entry(self, '', size=size, width=width)#,
+						#onchange=self._check_entry, onenter=self._entry_enter)
+				size = (-1, self.entry.GetSize()[1] - 1)
+				self.sb = SpinButton(self.entry, size=size)
+				w_pos = self.entry.GetSize()[0] - self.sb.GetSize()[0] - 2
+				self.sb.SetPosition((w_pos, -2))
+		else:
+			self.box = wx.BoxSizer(const.HORIZONTAL)
+			self.SetSizer(self.box)
+			self.entry = Entry(self, '', size=size, width=width)#,
+						#onchange=self._check_entry, onenter=self._entry_enter)
+			self.box.Add(self.entry, 0, wx.ALL)
 			size = (-1, self.entry.GetSize()[1])
-			self.sb = SpinButton(self, size=size)
-			w_pos = self.entry.GetSize()[0] - 5
-			self.sb.SetPosition((w_pos, -1))
-			line = HPanel(self)
-			line.SetSize((1, self.sb.GetSize()[1] - 1))
-			line.set_bg(const.UI_COLORS['dark_shadow'])
-			line.SetPosition((w_pos - 1, -1))
-		elif const.is_winxp():
-			self.entry = Entry(self)
-			size = (-1, self.entry.GetSize()[1]-1)
-			self.sb = SpinButton(self.entry, size=size)
-			w_pos = self.entry.GetSize()[0] - self.sb.GetSize()[0]-2
-			self.sb.SetPosition((w_pos, -2))
+			self.sb = SpinButton(self, size=size)#, onchange=self._check_spin)
+			self.box.Add(self.sb, 0, wx.ALL)
 
 
 class WidgetPanel(HPanel):
@@ -59,8 +88,8 @@ class WidgetPanel(HPanel):
 	def build(self):
 		flags = LEFT | CENTER
 		pflags = ALL | EXPAND
-		self.add(SpinControl(self),0,ALL,2)
-		self.add(SpinControl(self),0,ALL,2)
+		self.add(FloatSpin(self), 0, ALL, 2)
+		self.add(FloatSpin(self), 0, ALL, 2)
 
 
 app = Application('wxWidgets')
