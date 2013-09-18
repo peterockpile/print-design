e4048f04a323c649b3f0063119af9e246ecffeb6
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-09-18 09:01:09 +0000

    statusbar fontsize field

--- trunk/src/pdesign/app_conf.py
+++ trunk/src/pdesign/app_conf.py
@@ -108,6 +108,7 @@ class AppConfig(UCConfig):
 	mw_min_width = 1000
 	mw_min_height = 700
 	spin_overlay = True
+	statusbar_fontsize = '8'
 	palette = ''
 	show_splash = False
 	menu_size = (16, 16)
