61ba6fac1035b9edd7db8e6c86c4378452f30061
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-09-18 15:07:55 +0000

    history manager improvements to support detailed log

--- src/pdesign/app_history.py
+++ src/pdesign/app_history.py
@@ -17,7 +17,8 @@
 
 import os, time, datetime
 
-from pdesign import config
+from uc2.utils.fs import path_unicode
+from pdesign import config, appconst
 
 class AppHistoryManager:
 
@@ -33,58 +34,63 @@ class AppHistoryManager:
 
 	def read_history(self):
 		if os.path.isfile(self.history_file):
-			fp = open(self.history_file, 'wb')
+			fp = open(self.history_file, 'rb')
 			while True:
 				line = fp.readline()
 				if line == '': break
 				if line[-1:] == '\n': line = line[:-1]
 				items = line.split('\t')
-				if len(items) == 2:
-					self.history.append([items[0], int(items[1])])
+				if len(items) == 3:
+					self.history.append([int(items[0]), items[1], int(items[2])])
 			fp.close()
 
 	def save_history(self):
 		fp = open(self.history_file, 'wb')
 		for item in self.history:
-			fp.write(item[0] + '\t' + str(item[1]) + '\n')
+			fp.write(str(item[0]) + '\t' + item[1] + '\t' + str(item[2]) + '\n')
 		fp.close()
 
-	def add_entry(self, path):
+	def add_entry(self, path, operation=appconst.OPENED):
 		if not len(self.history) < config.history_size:
 			self.history = self.history[1:]
-		self.history.append(['' + path, int(time.time())])
+		self.history.append([operation, '' + path, int(time.time())])
 		self.save_history()
 
 	def clear_history(self):
 		self.history = []
 		self.save_history()
 
-	def is_empthy(self): return not self.history
+	def is_empty(self): return not self.history
 
 	def is_more(self): return len(self.history) > config.history_list_size
 
 	def get_menu_entries(self):
 		entries = []
-		if len(self.history) > config.history_list_size:
-			entries = self.history[-config.history_list_size:]
-		else:
-			entries += self.history
+		if not self.history: return entries
+		i = 1
+		counter = 0
 		ret = []
-		for item in entries:
-			path = item[0]
-			filename = os.path.basename(item[0])
-			ret.append([filename + ' [' + path + ']', path])
-		ret.reverse()
+		while counter < config.history_list_size:
+			item = self.history[-i]
+			if not item[1] in entries:
+				path = item[1]
+				entries.append(path)
+				filename = os.path.basename(path)
+				ret.append([path_unicode(filename + ' [' + path + ']'), path])
+				counter += 1
+			i += 1
+			if i > len(self.history):break
 		return ret
 
 	def get_history_entries(self):
 		ret = []
 		for item in self.history:
-			path = item[0]
-			filename = os.path.basename(item[0])
-			timestamp = datetime.datetime.fromtimestamp(item[1])
+			path = item[1]
+			unicode_path = path_unicode(path)
+			filename = path_unicode(os.path.basename(path))
+			timestamp = datetime.datetime.fromtimestamp(item[2])
 			timestr = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-			ret.append([filename, path, timestr])
+			ret.append([item[0], filename, unicode_path, path, timestr])
 		ret.reverse()
 		return ret
 
