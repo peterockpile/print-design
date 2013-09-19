118b05066151a4db0f45f97115daaa59949c8ce7
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2013-09-19 12:00:13 +0000

    view log dialog final implementation

--- trunk/src/pdesign/dialogs/log_viewer.py
+++ trunk/src/pdesign/dialogs/log_viewer.py
@@ -15,11 +15,13 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import wx
 
-from pdesign import _
+from pdesign import _, appconst
 from pdesign.widgets import BOTTOM, EXPAND, ALL, VERTICAL, HORIZONTAL
-from pdesign.widgets import const, HPanel, VPanel, Button
+from pdesign.widgets import const, HPanel, VPanel, Button, HLine
+from msgdlgs import error_dialog
 
 class LogViewerDialog(wx.Dialog):
 
@@ -28,63 +30,132 @@ class LogViewerDialog(wx.Dialog):
 	button_box = None
 	ok_btn = None
 	cancel_btn = None
+	lc = None
+	data = []
+	ret = ''
 
 	def __init__(self, parent, title, size=(500, 350)):
 
+		self.app = parent.app
+
 		wx.Dialog.__init__(self, parent, -1, title, wx.DefaultPosition, size)
 
-		self.sizer = wx.BoxSizer(wx.VERTICAL)
+		self.sizer = wx.BoxSizer(VERTICAL)
 		self.SetSizer(self.sizer)
 
-		margin = 5
-		if not const.is_gtk(): margin = 10
-
-		self.box = VPanel(self, border=BOTTOM, space=margin)
+		self.box = VPanel(self)
 		self.sizer.Add(self.box, 1, ALL | EXPAND)
 
 		self.build()
 
-		self.bottom_box = wx.BoxSizer(wx.HORIZONTAL)
+		self.box.add(HLine(self.box), 0, ALL | EXPAND)
+
+		self.bottom_box = HPanel(self)
 		self.sizer.Add(self.bottom_box, 0, EXPAND, 5)
-		self.clear_btn = wx.Button(self, -1, _('Clear history'), wx.DefaultPosition,
-							wx.DefaultSize, 0)
-		self.bottom_box.Add(self.clear_btn, 0, wx.ALL , 5)
+
+		self.clear_btn = Button(self.bottom_box, _('Clear history'),
+							onclick=self.clear_history)
+		self.bottom_box.add(self.clear_btn, 0, ALL , 5)
+
 		expander = HPanel(self)
-		self.bottom_box.Add(expander, 1, wx.ALL , 5)
+		self.bottom_box.add(expander, 1, ALL , 5)
 
-		self.button_box = wx.BoxSizer(wx.HORIZONTAL)
-		self.bottom_box.Add(self.button_box, 0, wx.ALL | wx.ALIGN_RIGHT, 5)
 
-		self.open_btn = wx.Button(self, wx.ID_OPEN, "", wx.DefaultPosition,
-							wx.DefaultSize, 0)
+		self.open_btn = wx.Button(self.bottom_box, wx.ID_OPEN, "",
+								wx.DefaultPosition, wx.DefaultSize, 0)
 		self.Bind(wx.EVT_BUTTON, self.on_open, self.open_btn)
 
-		self.cancel_btn = wx.Button(self, wx.ID_CANCEL, "", wx.DefaultPosition,
-								wx.DefaultSize, 0)
+		self.cancel_btn = wx.Button(self.bottom_box, wx.ID_CANCEL, "",
+								wx.DefaultPosition, wx.DefaultSize, 0)
 		self.Bind(wx.EVT_BUTTON, self.on_cancel, self.cancel_btn)
+
 		if const.is_gtk():
-			self.button_box.Add(self.cancel_btn, 0, wx.ALIGN_RIGHT)
-			self.button_box.Add(self.open_btn, 0, wx.ALIGN_RIGHT)
+			self.bottom_box.add(self.cancel_btn, 0, ALL, 5)
+			self.bottom_box.add(self.open_btn, 0, ALL, 5)
 		elif const.is_msw():
-			self.button_box.Add(self.open_btn, 0, ALL | wx.ALIGN_RIGHT)
-			self.button_box.Add(self.cancel_btn, 0, ALL | wx.ALIGN_RIGHT)
+			self.bottom_box.add(self.open_btn, 0, ALL, 5)
+			self.bottom_box.add(self.cancel_btn, 0, ALL, 5)
 		else:
-			self.button_box.Add(self.open_btn, 0, ALL | wx.ALIGN_RIGHT, 5)
-			self.button_box.Add(self.cancel_btn, 0, ALL | wx.ALIGN_RIGHT, 5)
+			self.bottom_box.add(self.open_btn, 0, ALL , 5)
+			self.bottom_box.add(self.cancel_btn, 0, ALL , 5)
+
 		self.cancel_btn.SetDefault()
+		self.open_btn.Enable(False)
+		self.update_list()
 
 	def build(self):
-		lc = wx.ListCtrl(self.box, -1, style=wx.LC_REPORT)
-		self.box.add(lc, 1, ALL | EXPAND)
+		margin = 0
+		if const.is_msw(): margin = 5
+		self.lc = wx.ListCtrl(self.box, -1, style=wx.LC_REPORT)
+		self.box.add(self.lc, 1, ALL | EXPAND, margin)
+		self.lc.Bind(wx.EVT_LIST_ITEM_SELECTED, self.update, self.lc)
+		self.lc.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.update, self.lc)
+		self.lc.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.on_open, self.lc)
+
+	def update(self, *args):
+		item = self.lc.GetFocusedItem()
+		if not item < 0:self.open_btn.Enable(True)
+		else:self.open_btn.Enable(False)
+		if self.data: self.clear_btn.Enable(True)
+		else:self.clear_btn.Enable(False)
+
+	def update_list(self):
+		self.lc.ClearAll()
+		self.lc.InsertColumn(0, _(' '))
+		self.lc.InsertColumn(1, _('File name'))
+		self.lc.InsertColumn(2, _('Path'))
+		self.lc.InsertColumn(3, _('Time'))
+		self.data = self.app.history.get_history_entries()
+
+		ldata = []
+		for item in self.data:
+			op = 'O'
+			if item[0] == appconst.SAVED:op = 'S'
+			ldata.append([op, item[1], item[2], item[4]])
+
+		odd_color = wx.Colour(240, 240, 240)
+		i = 0
+		odd = False
+		for item in ldata:
+			self.lc.Append(item)
+			if odd:
+				item = self.lc.GetItem(i)
+				item.SetBackgroundColour(odd_color)
+				self.lc.SetItem(item)
+			odd = not odd
+			i += 1
+
+		self.lc.SetColumnWidth(0, 20)
+		if self.data: self.lc.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+		else: self.lc.SetColumnWidth(1, 100)
+		self.lc.SetColumnWidth(2, 200)
+		self.lc.SetColumnWidth(3, 500)
+		self.update()
+
+	def clear_history(self, *args):
+		self.app.history.clear_history()
+		self.update_list()
 
 	def on_open(self, *args):
-		self.EndModal(wx.ID_OK)
+		item = self.lc.GetFocusedItem()
+		if not item < 0:
+			path = self.data[item][3]
+			if os.path.isfile(path):
+				self.ret = path
+				self.EndModal(wx.ID_OK)
+			else:
+				txt = "%s '%s' %s" % (_('File'), path, _('is not found.'))
+				error_dialog(self, _('File not found'), txt)
 
 	def on_cancel(self, event):
 		self.EndModal(wx.ID_CANCEL)
 
+	def get_result(self): return self.ret
+
 def log_viewer_dlg(parent):
+	ret = ''
 	dlg = LogViewerDialog(parent, _("Recent documents"))
 	dlg.Centre()
-	dlg.ShowModal()
+	if dlg.ShowModal() == wx.ID_OK: ret = dlg.get_result()
 	dlg.Destroy()
+	if ret:parent.app.open(ret)
