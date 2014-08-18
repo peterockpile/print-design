6e60b343870a8e2c66f3d038923bc44ad7aa2217
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2014-08-18 17:43:40 +0000

    basic structure of plugin area

--- src/pdesign/parts/plgarea.py
+++ src/pdesign/parts/plgarea.py
@@ -15,17 +15,27 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pdesign.widgets import VPanel, ALL, EXPAND
+import wx
+from pdesign.widgets import HPanel, VPanel, ALL, EXPAND, VLine
 
-class PlgArea(VPanel):
+class PlgArea(HPanel):
 
 	app = None
 	active_plg = None
 	plugins = []
+	container = None
+	tabs = None
 
 	def __init__(self, app, parent):
 		self.app = app
-		VPanel.__init__(self, parent)
+		HPanel.__init__(self, parent)
+		line = VLine(self)
+		self.add(line, 0, ALL | EXPAND)
+		self.container = VPanel(self)
+		self.add(self.container, 1, ALL | EXPAND)
+		self.tabs = PlgTabsPanel(app, self)
+		self.add(self.tabs, 0, ALL | EXPAND)
+		self.Layout()
 
 	def check_id(self, pid):
 		for item in self.plugins:
@@ -46,8 +56,22 @@ class PlgArea(VPanel):
 			self.active_plg.hide()
 		if not item:
 			item = self.load_plugin(pid)
-			self.add(item.panel, 1, ALL | EXPAND)
+			self.container.add(item.panel, 1, ALL | EXPAND)
 		self.active_plg = item
 		self.active_plg.show()
+		self.container.Layout()
 		self.Layout()
 
+class PlgTabsPanel(VPanel):
+
+	app = None
+	active_plg = None
+	plugins = []
+	container = None
+	tabs = None
+
+	def __init__(self, app, parent):
+		self.app = app
+		VPanel.__init__(self, parent)
+		self.SetBackgroundColour(wx.Colour(183, 183, 183))
+		self.add((10, 10))
