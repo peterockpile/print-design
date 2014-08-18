269ffe6bd18464a8226749b09ef03a06e70a1a73
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2014-08-18 17:44:19 +0000

    minor improvements

--- trunk/src/pdesign/share/pd_plugins/test_plugin/__init__.py
+++ trunk/src/pdesign/share/pd_plugins/test_plugin/__init__.py
@@ -1,6 +1,8 @@
 
 from pdesign.app_plugins import RS_Plugin
 
+from pdesign.widgets import Label, CENTER
+
 def get_plugin(app):
 	return Test_Plugin(app)
 
@@ -8,3 +10,8 @@ class Test_Plugin(RS_Plugin):
 
 	pid = 'TestPlugin'
 	name = 'Test Plugin'
+
+	def build_ui(self):
+		label = Label(self.panel, self.name, True, 2, (255, 0, 0))
+		self.panel.add(label, 0, CENTER, 5)
+
