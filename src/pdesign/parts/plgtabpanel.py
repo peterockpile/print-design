378ae67523a16d9a0a2e8dbf387114bc7e3a738f
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@ad05ea04-73d8-69e9-ff70-d3b301749253>
2014-08-21 15:35:11 +0000

    PlgTab rendering

--- src/pdesign/parts/plgtabpanel.py
+++ src/pdesign/parts/plgtabpanel.py
@@ -160,12 +160,16 @@ class PlgTab(VPanel):
 		self.parent = parent
 		self.active = active
 		self.text = self.plg.name
-		self.icon = wx.ArtProvider.GetBitmap(icons.DOCUMENT_ICON)
+		self.icon = wx.ArtProvider.GetBitmap(icons.PLUGIN_ICON)
 		self.close_but = wx.ArtProvider.GetBitmap(icons.PD_CLOSE_BUTTON_ACTIVE)
 		self.inactive_close_but = wx.ArtProvider.GetBitmap(icons.PD_CLOSE_BUTTON)
 		VPanel.__init__(self, parent)
 		self.add((TAB_WIDTH, self.get_best_height()))
 		self.Bind(wx.EVT_PAINT, self._on_paint, self)
+		self.Bind(wx.EVT_MOTION, self._on_move, self)
+		self.Bind(wx.EVT_LEFT_DOWN, self._on_left_down, self)
+		self.Bind(wx.EVT_LEFT_UP, self._on_left_up, self)
+		self.Bind(wx.EVT_LEAVE_WINDOW, self._on_win_leave, self)
 
 	def set_title(self, title):
 		self.text = title
@@ -209,6 +213,40 @@ class PlgTab(VPanel):
 
 	######################################
 
+	def _on_move(self, event):
+		mouse_pos = event.GetPosition()
+		if self.but_rect.Inside(mouse_pos) and not self.but_active:
+			self.but_active = True
+			self.refresh()
+		if not self.but_rect.Inside(mouse_pos) and self.but_active:
+			self.but_active = False
+			self.but_pressed = False
+			self.refresh()
+
+	def _on_win_leave(self, event):
+		if self.but_active:
+			self.but_active = False
+			self.but_pressed = False
+			self.refresh()
+
+	def _on_left_down(self, event):
+		mouse_pos = event.GetPosition()
+		if self.but_rect.Inside(mouse_pos):
+			self.but_pressed = True
+			self.refresh()
+
+	def _on_left_up(self, event):
+		mouse_pos = event.GetPosition()
+		if self.but_rect.Inside(mouse_pos):
+			self.but_pressed = False
+			self.refresh()
+#			self.doc.app.close(self.doc)
+		elif self.rect.Inside(mouse_pos) and not self.active:
+			pass
+#			self.doc.app.set_current_doc(self.doc)
+
+	######################################
+
 	def _on_paint(self, event):
 		w, h = self.panel.GetSize()
 		self.rect = wx.Rect(0, 0, w, h)
@@ -217,6 +255,75 @@ class PlgTab(VPanel):
 		except:dc = pdc
 		pdc.BeginDrawing()
 		dc.BeginDrawing()
+
+		#----- colors definition
+		border_color = const.UI_COLORS['hover_solid_border']
+		bg_color = const.UI_COLORS['bg']
+		light_bg_color = const.mix_colors((255, 255, 255), bg_color, 100)
+		dark_bg_color = const.mix_colors((0, 0, 0), bg_color, 5)
+		grad_start = wx.Colour(0, 0, 0, 10)
+		grad_end = wx.Colour(0, 0, 0, 0)
+
+		if self.active:
+			#----- draw border
+			pdc.SetBrush(wx.TRANSPARENT_BRUSH)
+			pdc.SetPen(wx.Pen(wx.Colour(*border_color), 1))
+			pdc.DrawRoundedRectangle(-3, 0, w, h, 3.0)
+			#----- draw icon
+			y = TAB_PADDING
+			x = (TAB_WIDTH - self.icon.GetSize()[1]) / 2
+			dc.DrawBitmap(self.icon, x, y, True)
+			#----- draw text
+			y += 3 + self.icon.GetSize()[0]
+			txt_h = self._get_text_size(self.text, self.active)[1]
+			x = (TAB_WIDTH - txt_h) / 2 + txt_h - 2
+			font = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
+			font.SetWeight(wx.FONTWEIGHT_BOLD)
+			pdc.SetFont(font)
+			pdc.DrawRotatedText(self.text, x, y, 270)
+			#----- draw button
+			y += self._get_text_size(self.text, self.active)[0]
+			x = (TAB_WIDTH - self.inactive_close_but.GetSize()[0]) / 2
+			if not self.active:x -= 1
+			if self.but_active:
+				dc.DrawBitmap(self.close_but, x, y, True)
+				if self.but_pressed: dc.DrawBitmap(self.close_but, x, y, True)
+			else: dc.DrawBitmap(self.inactive_close_but, x, y, True)
+			but_w, but_h = self.inactive_close_but.GetSize()
+			self.but_rect = wx.Rect(x, y, but_w, but_h)
+		else:
+			#----- draw border
+			pdc.SetBrush(wx.TRANSPARENT_BRUSH)
+			pdc.SetPen(wx.Pen(wx.Colour(*border_color), 1))
+			pdc.DrawRoundedRectangle(-5, 0, w, h, 3.0)
+			pdc.SetPen(wx.Pen(wx.Colour(*const.UI_COLORS['hover_solid_border']), 1))
+			pdc.DrawLine(0, 0, 0, h)
+			#----- draw icon
+			y = TAB_PADDING
+			x = (TAB_WIDTH - self.icon.GetSize()[1]) / 2 - 1
+			dc.DrawBitmap(self.icon, x, y, True)
+			#----- draw text
+			y += 3 + self.icon.GetSize()[0]
+			txt_h = self._get_text_size(self.text, self.active)[1]
+			x = (TAB_WIDTH - txt_h) / 2 + txt_h - 3
+			font = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
+			pdc.SetFont(font)
+			pdc.DrawRotatedText(self.text, x, y, 270)
+			#----- draw button
+			y += self._get_text_size(self.text, self.active)[0]
+			x = (TAB_WIDTH - self.inactive_close_but.GetSize()[0]) / 2
+			if not self.active:x -= 1
+			if self.but_active:
+				dc.DrawBitmap(self.close_but, x, y, True)
+				if self.but_pressed: dc.DrawBitmap(self.close_but, x, y, True)
+			else: dc.DrawBitmap(self.inactive_close_but, x, y, True)
+			but_w, but_h = self.inactive_close_but.GetSize()
+			self.but_rect = wx.Rect(x, y, but_w, but_h)
+			#----- shadow
+			color1 = wx.Colour(0, 0, 0, 20)
+			color2 = wx.Colour(0, 0, 0, 0)
+			rect = wx.Rect(0, 0, w / 2, h)
+			dc.GradientFillLinear(rect, color1, color2, nDirection=wx.EAST)
 
 
 		if not pdc == dc:
